74fcb50d444d3e98fbf1df712d4cbf9935d05b1c
stoke <stoke95@yahoo.it>
2012-06-11 13:19:18 +0200

    Implemented middleware for handling basic authentication
    Implemented middleware for handling basic authentication

--- app.js
+++ app.js
@@ -2,7 +2,24 @@ var flatiron = require('flatiron'),
     links    = require('./lib/links'),
     app      = flatiron.app;
   
-app.use(flatiron.plugins.http);
+app.use(flatiron.plugins.http, {
+  before: [function(req, res, next) { // handles basic auth
+    var head, auth, username, password; 
+
+    if (typeof req.headers.authorization === 'undefined')
+      return next();
+
+    head = req.headers.authorization.split(" ")[1];
+    auth = new Buffer(head, 'base64').toString('utf8');
+    username = auth.split(":")[0];
+    password = auth.split(":")[1];
+
+    req.username = username;
+    req.password = password;
+
+    return next();
+  }]
+});
 
 app.router.get('/', function () {
   this.res.writeHead(200);
@@ -10,6 +27,6 @@ app.router.get('/', function () {
 });
 
 app.router.get('/links', links.all);
-
+app.router.get('/links/:id', links.get);
 
 app.start(8080);
