4a5e1e594f968fa35ccc0d1d8ff970ad61e206c7
stoke <stoke95@yahoo.it>
2012-06-12 17:03:02 +0200

    create for links, users, tests

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 var flatiron = require('flatiron'),
     links    = require('./lib/links'),
+    users    = require('./lib/users'),
+    connect  = require('connect'),
     app      = flatiron.app;
   
 app.use(flatiron.plugins.http, {
-  before: [function(req, res, next) { // handles basic auth
-    var head, auth, username, password; 
+  before: [
+    function(req, res, next) { // handles basic auth
+      var head, auth, username, password; 
 
-    if (typeof req.headers.authorization === 'undefined')
+      if (typeof req.headers.authorization === 'undefined')
+        return next();
+
+      head = req.headers.authorization.split(' ')[1] || '';
+      auth = new Buffer(head, 'base64').toString('utf8');
+      username = auth.split(":")[0];
+      password = auth.split(":")[1];
+
+      if (!username || !password)
+        return next();
+
+      req.username = username;
+      req.password = password;
+
       return next();
-
-    head = req.headers.authorization.split(" ")[1];
-    auth = new Buffer(head, 'base64').toString('utf8');
-    username = auth.split(":")[0];
-    password = auth.split(":")[1];
-
-    req.username = username;
-    req.password = password;
-
-    return next();
-  }]
+    },
+    connect.cookieParser(),
+    connect.session({ key: 'sid', secret: 'herpderp', cookie: { maxAge: 60000 } })
+  ]
 });
 
 app.router.get('/', function () {
@@ -27,6 +36,10 @@ app.router.get('/', function () {
 });
 
 app.router.get('/links', links.all);
+app.router.post('/links', links.create);
 app.router.get('/links/:id', links.get);
+
+app.router.post('/users', users.create);
+app.router.get('/users/login', users.login);
 
 app.start(8080);
--- models/user.js
+++ models/user.js
@@ -1,10 +1,14 @@
+var crypto = require('crypto');
+
 module.exports = function() {
   this.string('username', {
     required: true,
     
-    pattern: /^[A-Za-z0-9_!@\^\-\$\.]{0,23}$/
+    pattern: /^[A-Za-z0-9_!@\^\-\$\.]{0,23}$/,
+    unique: true
   });
   
+  this.string('salt', {required: true})
   this.string('password', {required: true});
   
   this.timestamps();
