95a5601675ad6aba319bf05be2df7414dcf3221e
yawnt <yawn.localhost@gmail.com>
2012-07-17 17:46:58 +0200

    [feature] client.create()

--- models/user.js
+++ models/user.js
@@ -1,13 +1,6 @@
 var crypto = require('crypto');
 
 module.exports = function() {
-  this.string('username', {
-    required: true,
-    
-    pattern: /^[A-Za-z0-9_!@\^\-\$\.]{0,23}$/,
-    unique: true
-  });
-  
   this.string('salt', {required: true})
   this.string('password', {required: true});
   
