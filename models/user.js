91711b066364b10eb24dea1ddaf3285765bc32b7
yawnt <yawn.localhost@gmail.com>
2012-07-19 14:37:20 +0200

    [feature] email and name

--- models/user.js
+++ models/user.js
@@ -3,6 +3,8 @@ var crypto = require('crypto');
 module.exports = function() {
   this.string('salt', {required: true})
   this.string('password', {required: true});
+  this.string('email', {required: true});
+  this.string('name', {required: true});
   
   this.timestamps();
 };
