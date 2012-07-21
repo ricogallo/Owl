7aad054292efcc0fc492f92c297a781e61b7b627
stoke <stoke95@yahoo.it>
2012-07-21 15:21:33 +0200

    [test] Users test now working

--- test/users-test.js
+++ test/users-test.js
@@ -1,26 +1,41 @@
-var APIeasy     = require('api-easy'),
-    assert      = require('assert'),
-    toBasicAuth = require('./helpers').toBasicAuth;
+var APIeasy   = require('api-easy'),
+  assert      = require('assert'),
+  expect      = require('expect.js'),
+  toBasicAuth = require('./helpers').toBasicAuth;
 
-var suite = APIeasy.describe('users.js');
+var Browser = require('zombie');
+var assert = require('assert');
 
-suite
-     .use('localhost', 8080)
-     .setHeader('Content-Type', 'application/json')
-     .post('/users', {username: 'test'})
-        .expect(400)
-     .next()
-     .post('/users', {username: '###', password: 'test'})
-        .expect(500)
-     .next()
-     .post('/users', {username: 'test', password: 'test'})
-        .expect(201)
-     .next()
-     .setHeader('Authorization', toBasicAuth('nonexistentuser', 'nonexistentpassword'))
-     .get('/links')
-        .expect(401)
-     .next()
-     .setHeader('Authorization', toBasicAuth('test', 'test'))
-     .get('/links')
-        .expect(200)
-     .export(module);
+
+browser = new Browser();
+
+describe('user.js', function() {
+  it('should register', function(done) {
+    browser.visit('http://localhost:8000/sign_up/', function () {
+      browser.
+      fill('username', 'test').
+      fill('password', 'test').
+      fill('name', 'test').
+      fill('surname', 'test').
+      fill('email', 'test@test.te').
+
+      pressButton('Sign Up!', function() {
+        expect(browser.success).to.be(true)
+        done();
+      });
+    });
+  });
+  
+  it('should login', function(done) {
+    browser.visit('http://localhost:8000/login', function() {
+      browser.
+        fill('username', 'test').
+        fill('password', 'test').
+
+        pressButton('Sign in', function() {
+          expect(browser.success).to.be(true);
+          done();
+        });
+    });
+  });
+});
