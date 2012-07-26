2f9e5bc6c4870972d42b16cb0ded1e37f3d42343
stoke <stoke95@yahoo.it>
2012-07-26 16:34:55 +0200

    [dist] User api

--- core/users.js
+++ core/users.js
@@ -3,78 +3,44 @@ var models = require('../models/'),
 
 var users = exports;
 
-users.create = function(req, res) {
-  var username = req.body.username,
-      password = req.body.password,
-      name = req.body.name,
-      surname = req.body.surname,
-      email = req.body.email,
-      salt = common.salt();
-
-  if (
-    typeof username === 'undefined' ||
-    typeof password === 'undefined' ||
-    typeof name === 'undefined'     ||
-    typeof surname === 'undefined'  ||
-    typeof email === 'undefined'
-  ) return res.send(400);
-
-  password = common.crypt(salt + password);
-
-  models.User.create({id: username, password: password, salt: salt, name: name+' '+surname, email: email}, function(err, docs) {
-    //
-    // If there's an error return a 500
-    //
-    if (err) {
-      res.send(500);
-    }
-    return res.redirect('/');
-  });
-};
-
-users.get = function(req, res) {
-  var id = req.params.id,
-      whitelist = ['email', 'name'];
+users.get = function(id, callback) {
+  var whitelist = ['email', 'name'];
 
   models.User.get(id, function(err, user) {
     var json = {};
 
     if (err) {
       if (err.error && err.error === 'not_found')
-        return res.end(res.writeHead(404));
+        return callback(new Error(404));
       else
-        return res.end(res.writeHead(500));
+        return callback(new Error(500));
     }
-      
-
-    if (!user)
-      
 
     whitelist.forEach(function(x) {
       json[x] = user[x];
     });
 
-    res.json(json);
+    callback(err, json);
   });
 }
 
-users.me = function(req, res) {
+users.me = function(user, callback) {
   var whitelist = whitelist = ['email', 'name', 'username'];
 
-  models.User.get(req.user.id, function(err, docs) {
+  models.User.get(user.id, function(err, docs) {
     var json = {};
 
     if (err) {
       if (err.error && err.error === 'not_found')
-        return res.end(res.writeHead(404));
+        return callback(new Error(404));
       else
-        return res.end(res.writeHead(500));
+        return callback(new Error(500));
     }
 
     whitelist.forEach(function(x) {
       json[x] = user[x];
     });
 
-    res.json(json);
+    callback(err, json);
   });
 }
