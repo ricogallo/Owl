eb23a1a58dc4cb0dd25d03bc54c642b692be0ead
yawnt <yawn.localhost@gmail.com>
2012-07-26 16:36:44 +0200

    [minor] move old file

---
+++ web/lib/client.js
@@ -0,0 +1,29 @@
+var models = require('../models/'),
+    common = require('./common');
+
+var client = exports;
+
+client.create = function(req, res) {
+  var client_id    =  req.body.client_id,
+      redirect_uri =  req.body.redirect_uri;
+  
+  if(
+    typeof client_id === 'undefined' ||
+    typeof redirect_uri === 'undefined'
+  ) return res.end(res.writeHead(400)); 
+  
+  models.Client.create({
+    id: client_id,
+    user_id: req.user.id,
+    redirect_uri: redirect_uri,
+    client_secret: common.client_secret()
+  }, function(err, client) {
+    if(err) return res.send(500);
+
+    res.redirect('/');
+  });
+};
+
+client.createForm = function(req, res) {
+  res.render('client', {user: req.user});
+};
