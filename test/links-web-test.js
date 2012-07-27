f13122457a4a77ce6ac7a966e05d5f7a685672ce
yawnt <yawn.localhost@gmail.com>
2012-07-27 14:57:50 +0200

    [test] raise timeout due to cdn scripts

--- test/links-web-test.js
+++ test/links-web-test.js
@@ -9,16 +9,16 @@ describe('links.js', function() {
 
   it('should prepare test stubs', function(done) {
     browser.visit('http://localhost:8000/sign_up/', function () {
-      browser.
-      fill('username', 'test').
-      fill('password', 'test').
-      fill('name', 'test').
-      fill('surname', 'test').
-     fill('email', 'test@test.te').
-      pressButton('Sign Up', function() {
-        expect(browser.success).to.be(true);
-        done();
-      });
+      browser
+        .fill('username', 'test')
+        .fill('password', 'test')
+        .fill('name', 'test')
+        .fill('surname', 'test')
+        .fill('email', 'test@test.te')
+        .pressButton('Sign Up', function() {
+          expect(browser.success).to.be(true);
+          done();
+        });
     });
   });
 
@@ -30,7 +30,7 @@ describe('links.js', function() {
         .fill('password', 'test')
         .pressButton('Sign in', function() {
           assert.equal(browser.success, true);
-           
+          console.log('here'); 
           browser.visit('http://localhost:8000/new', function() {
             browser
               .fill('uri', 'http://testalicious.com')
