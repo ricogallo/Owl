eda99b38730bbb93db4bf8727c2eaf4178bf0e7c
stoke <stoke95@yahoo.it>
2012-08-30 17:43:08 +0200

    [fix] models

--- models/client.js
+++ models/client.js
@@ -5,7 +5,7 @@ module.exports = {
   'client_id': hater.Types.String(),
   'redirect_uri': hater.Types.String(),
   'client_secret': hater.Types.String(),
-  'user_id': hater.Types.String()
+  'user_id': hater.Types.Integer()
   // timestamps
 };
 
