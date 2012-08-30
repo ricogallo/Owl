a44a86672e1d337f6130b93f4b10998b25a66f7f
stoke <stoke95@yahoo.it>
2012-08-30 17:59:36 +0200

    [fix] email regexp

--- models/user.js
+++ models/user.js
@@ -4,6 +4,6 @@ module.exports = {
   'username': hater.Types.String({validate: {minLength: 5, maxLength: 20}}),
   'salt': hater.Types.String(),
   'password': hater.Types.String(),
-  'email': hater.Types.String({validate: {pattern: /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/}}),
+  'email': hater.Types.String({validate: {pattern: /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/}}),
   'name': hater.Types.String({validate: {pattern: /[a-zA-Z]+ [a-zA-Z]+/}})
 };
--- test/links-api-test.js
+++ test/links-api-test.js
@@ -8,7 +8,7 @@ var uid;
 
 before(function(done) {
   var user = {username: 'testusername', password: common.crypt('ohaiu' + 'test'), salt: 'ohaiu', email: 'test@testest.te', name: 'lol lol'};
-  var user1 = {username: '1testusername', password: common.crypt('ohaiu' + 'test'), salt: 'ohaiu', email: 'test@testest.1te', name: 'lol lol'};
+  var user1 = {username: '1testusername', password: common.crypt('ohaiu' + 'test'), salt: 'ohaiu', email: 'test@testest.ote', name: 'lol lol'};
   var client = {client: 'buh', redirect_uri: 'http://localhost:8080/callback', client_secret: 'keyboardcat', user_id: 'testusername'};
   var code = {code: 'code', client_id: 'buh', redirect_uri: 'http://localhost:8080/callback', client_secret: 'keyboardcat', user_id: 'testusername'};
   var token = {token: 'testoken', client_id: 'buh', user_id: 'testusername'};
