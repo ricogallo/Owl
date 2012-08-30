fcac3c89623ffba0fd83cbaa46c5eba2123cc8ae
stoke <stoke95@yahoo.it>
2012-08-30 19:32:22 +0200

    [fix] client

--- models/client.js
+++ models/client.js
@@ -1,11 +1,9 @@
 var hater = require('hater');
 
 module.exports = {
-  'client': hater.Types.String(),
   'client_id': hater.Types.String(),
   'redirect_uri': hater.Types.String(),
   'client_secret': hater.Types.String(),
   'user_id': hater.Types.Integer()
   // timestamps
 };
-
