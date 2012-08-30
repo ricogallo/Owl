b62e3f42f547f4c13c4ab386d1c28d009a22f928
stoke <stoke95@yahoo.it>
2012-08-30 17:39:49 +0200

    [feature] validation

--- models/user.js
+++ models/user.js
@@ -1,9 +1,9 @@
 var hater = require('hater');
 
 module.exports = {
-  'username': hater.Types.String(),
+  'username': hater.Types.String({validate: {minLength: 5, maxLength: 20}}),
   'salt': hater.Types.String(),
   'password': hater.Types.String(),
-  'email': hater.Types.String(),
-  'name': hater.Types.String()
+  'email': hater.Types.String({validate: {pattern: /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/}}),
+  'name': hater.Types.String({validate: {pattern: /[a-zA-Z]+ [a-zA-Z]+/}})
 };
