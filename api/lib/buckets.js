487e3abfd71d0af76275f840afd97ff469d0dff1
yawnt <yawn.localhost@gmail.com>
2012-09-01 21:56:17 +0200

    [fix] refactor

--- api/lib/buckets.js
+++ api/lib/buckets.js
@@ -4,25 +4,25 @@ var core        = require('../../core/buckets'),
 var buckets = exports;
 
 buckets.create = function(req, res) {
-  core.create({ user: req.user, name: req.body.name }, handleError(function(_, bucket) {
+  core.create({ user: req.user, name: req.body.name }, handleError(res, function(_, bucket) {
     res.send(201); 
   }));
 };
 
 buckets.addLink = function(req, res) {
-  core.addLink(req.body, handleError(function() {
+  core.addLink(req.body, handleError(res, function() {
     res.send(204);
   }));
 };
 
 buckets.show = function(req, res) {
-  core.show(req.params, handleError(function(_, buckets) {
+  core.show(req.params, handleError(res, function(_, buckets) {
     res.json(buckets);
   }));
 };
 
 buckets.showOne = function(req, res) {
-  core.showOne(req.params, handleError(function(_, bucket) {
+  core.showOne(req.params, handleError(res, function(_, bucket) {
     res.json(bucket);
   }));
 };
